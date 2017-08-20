723071b10a51e7cf58da2e9e657c4cc3265beff8
pianohagens <pianohagens@gmail.com>
2017-08-19 22:02:24 -0700

    aug_19th_completed
    removed sidebar from contact page and style contact page individually, added svg Washington state Map, enable to create sidebar for each page as needed.

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div>
+<!-- End Content -->
+
+<!-- Begin Footer &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;    &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; <?php /*wp_loginout();*/?>  &nbsp;  &nbsp; <a href="#">FAQ</a> -->
+<div id="footer">
+    <p class="cyright">&copy;2017     | The Winding Path  |  All Right Reserved  </p>
+
+<!-- End Footer -->
+
+<!-- Start WP Footer -->
+<?php wp_footer(); ?>
+<!-- End WP Footer -->
+
+</body>
+</html>
